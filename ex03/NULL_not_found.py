7b61abb76721a67f05044eea03db7bfcf4a4b022
Rodrigo Cruz Antunes <rcruz-an@c2r1s6.42porto.com>
2026-04-30 12:24:58 +0100

    Ex03 done

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,1 +1,16 @@
-def NULL_not_found
+def NULL_not_found(object: any) -> int:
+    obj_type = type(object)
+
+    if object is None:
+        print(f"Nothing : None {obj_type}")
+    elif obj_type == float and object != object:
+        print(f"Cheese : nan {obj_type}")
+    elif obj_type == int and object == 0:
+        print(f"Zero : 0 {obj_type}")
+    elif obj_type == str and object == "":
+        print(f"Empty : {obj_type}")
+    elif obj_type == bool and object == False:
+        print(f"Fake : False {obj_type}")
+    else:
+        print("Type not found")
+    return 1
