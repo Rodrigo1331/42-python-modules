548c1d49158a0f568436bd51f15177751b0bda3b
Rodrigo1331 <rodrigocruzantunes@gmail.com>
2026-05-04 10:52:41 +0000

    ex05 done, and virtual enviroment set up done

---
+++ ex05/building.py
@@ -0,0 +1,51 @@
+import sys
+
+
+def analyze_text(text):
+    """Counting character types in argv[1]"""
+
+    upper = 0
+    lower = 0
+    punctuation = 0
+    spaces = 0
+    digits = 0
+
+    for char in text:
+        if char.isupper():
+            upper += 1
+        elif char.islower():
+            lower += 1
+        elif not char.isalnum() and not char.isspace():
+            punctuation += 1
+        elif char.isspace():
+            spaces += 1
+        elif char.isdigit():
+            digits += 1
+
+    total = len(text)
+
+    print(f"The text contains {total} characters:")
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{punctuation} punctuation marks")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
+
+
+def main():
+    """Checking if its a valid prompt"""
+    argc = len(sys.argv)
+
+    if argc == 1:
+        return
+    elif argc == 2:
+        text = sys.argv[1]
+    else:
+        print("AssertionError: more than one argument is provided")
+        return
+
+    analyze_text(text)
+
+
+if __name__ == "__main__":
+    main()
